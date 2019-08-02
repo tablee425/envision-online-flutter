2838e9f9e446394e6eb18fd1994f19d7b96cb386
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-08-02 08:41:48 +0400

    initialize splash page

---
+++ envision_online/lib/pages/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
