5c6e1b92614b97fcbec2feffc441598629e16941
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-08-05 11:23:17 +0400

    api request for area fetch

---
+++ envision_online/lib/models/ApiRequest_Area.dart
@@ -0,0 +1,26 @@
+import 'Area.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+ApiRequest_Area _$ApiRequest_AreaFromJson(Map<String, dynamic> json) => new ApiRequest_Area(
+  company_id: json['company_id'] as int,
+  user_id: json['user_id'] as int,
+);
+
+abstract class _$ApiRequest_AreaSerializerMixin {
+  int get company_id;
+  int get user_id;
+
+  Map<String, dynamic> toJson() => <String, dynamic> {
+    'company_id': company_id,
+    'user_id': user_id,
+  };
+}
+
+@JsonSerializable()
+class ApiRequest_Area extends Object with _$ApiRequest_AreaSerializerMixin {
+  int company_id;
+  int user_id;
+
+  ApiRequest_Area({this.company_id, this.user_id});
+  factory ApiRequest_Area.fromJson(Map<String, dynamic> json) => _$ApiRequest_AreaFromJson(json);
+}
